46446944116c2e170991236d7e450c679aff2093
hanzala bawany <hanzalabawany0@gmail.com>
2025-02-17 10:50:25 +0500

    my first caculator

---
+++ app.js
@@ -0,0 +1,10 @@
+let changeThemeBtn = document.body.querySelector("#darkAndLightImg");
+let calculator = document.body.querySelector("#calculator");
+let lightTheme = true;
+// let darkTheme = false;
+
+changeThemeBtn.addEventListener("click",function(){
+calculator.style.backgroundColor = "rgba(194, 191, 191, 0.699)";
+
+
+})
